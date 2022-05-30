7e15b037cc5ab15b532ce6f1cbcb55431b6a0cc7
NHawk257 <90468946+NHawk257@users.noreply.github.com>
2022-05-30 10:55:29 -0600

    Created CopyGroupMembers

---
+++ AD/CopyGroupMembers.ps1
@@ -0,0 +1,29 @@
+Connect-AzureAD
+
+# Move all the people in the group called: ES_Office 365 E5 License 
+# To the group called: ES_Microsoft 365 E5 License
+
+$OldGroup = Get-azureAdGroup -SearchString "ES_Office 365 E5 License" | Select -ExpandProperty ObjectId
+$OldMembers = Get-AzureADGroupMember -ObjectId $OldGroup -All $True | Select -ExpandProperty ObjectId
+
+$NewGroup = Get-AzureADGroup -SearchString "ES_Microsoft 365 E5 License" | Select -ExpandProperty ObjectId
+$NewMembers = Get-AzureADGroupMember -ObjectId $NewGroup -All $True | Select -ExpandProperty ObjectId
+
+
+foreach ($OldMember in $OldMembers){
+
+    Add-AzureADGroupMember -ObjectId $NewGroup -RefObjectId $OldMember
+
+}
+
+#Double-checking counts make sense
+$NewNewMembers = Get-AzureADGroupMember -ObjectId $NewGroup -All $True | Select -ExpandProperty ObjectId
+
+Write-Host "Old Count was:"
+$OldMembers.Count
+Write-Host "New Count was:"
+$NewMembers.Count
+Write-Host "New New Count should be:"
+($OldMembers.count) + ($NewMembers.count)
+Write-Host "New Count is:"
+$NewNewMembers.Count
