f5231d553fbd1cf24ab625b84c478d8391209a10
NHawk257 <90468946+NHawk257@users.noreply.github.com>
2022-07-12 13:54:48 -0600

    Fixed msol command

--- AD/Graph-GetMFAStatusReport.ps1
+++ AD/Graph-GetMFAStatusReport.ps1
@@ -38,7 +38,7 @@ Function Connect_MgGraph
 Connect_MgGraph
 
 #Import/connect to MSOL. The MS Graph Module doesn't give Admin roles currently due to a bug
-Import-Module MsolService -UseWindowsPowerShell #For VSC/PS7 purposes, this isn't needed for regular PS
+Import-Module MSOnline -UseWindowsPowerShell #For VSC/PS7 purposes, this isn't needed for regular PS
 Connect-Msonline
 
 #Verify connected and advise the user of the connected context
