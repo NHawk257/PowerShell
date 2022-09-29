99083fe43a0e3a45838b9d461444a6a1756270c6
NHawk257 <90468946+NHawk257@users.noreply.github.com>
2022-09-29 12:13:21 -0600

    Updated DL Reporting

---
+++ Exchange/Get all DL members.ps1
@@ -0,0 +1,16 @@
+﻿Connect-ExchangeOnline
+
+$csvfilename = ".\DL_Members.csv"
+New-Item $csvfilename -type file -force
+Add-Content $csvfilename "Name,Owner,Members"
+
+$Groups = Get-DistributionGroup | Select-Object -ExpandProperty PrimarySmtpAddress
+
+foreach ($Group in $groups){
+
+$Owner = Get-DistributionGroup $Group | Select-Object -ExpandProperty ManagedBy
+$members = Get-DistributionGroupMember $Group | Select-Object -ExpandProperty Name
+
+Add-Content $csvfilename "$group,$owner,$members"
+
+}
