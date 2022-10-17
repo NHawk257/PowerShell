b8131f293cbd94e1787d8b153899543694c6e65b
NHawk257 <90468946+NHawk257@users.noreply.github.com>
2022-10-17 11:42:02 -0600

    Delete Finning directory

--- Finning/Connect-OnPrem.ps1
+++
@@ -1,3 +0,0 @@
-$UserCredential = Get-Credential
-$Session = New-PSSession -ConfigurationName Microsoft.Exchange -ConnectionUri http://pr-exch-vm01.enterprise.finning.com/PowerShell/ -Authentication Kerberos -Credential $UserCredential
-Import-PSSession $Session -DisableNameChecking
