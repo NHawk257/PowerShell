0cde9c7db543b6bc1dc76700d37c360cc527e69e
NHawk257 <90468946+NHawk257@users.noreply.github.com>
2022-11-09 11:19:27 -0700

    Notes and cleaning

--- Exchange/Bulk-Create-Mailbox.ps1
+++ Exchange/Bulk-Create-Mailbox.ps1
@@ -1,12 +1,15 @@
 Connect-ExchangeOnline
 
-#Input the CSV name/path:
-$CSVPath = "C:\Users\Evan\Documents\Cubiq_Accounts.csv"
-$Members = @('kim.ng@finning.com','pentti.huttunen@finning.com','nick.rao@finning.com','Oscar.Palacio@finning.com','jessicapaola.gaytan@finning.com')
-
+<# Parameters Note:
+CSV file should be created with two columns, Name and Email. 
+The Name column will be used for the DisplayName and Name values of the mailbox. The Email will be used for the Primary SMTP
+The Members Aray should be a list of all users (Alias or SMTP) that will be given permissions to this mailbox
+#>
+$CSVPath = ".\FilePath.csv"
+$Members = @('Member1','Member2','Member3')
 $Mailboxes = Import-Csv $CSVPath
 
-#Check if mailboxes exist first (double-checking to verify no issues in creating mailboxes)
+#Check if mailboxes exist first to stop errors. Also useful to verify creations worked OK
 Foreach ($Mailbox in $mailboxes){
 
     try {
@@ -27,14 +30,18 @@ Foreach ($Mailbox in $mailboxes){
 #Create Mailboxes
 Foreach ($Mailbox in $mailboxes){
 
-    Write-host ""$Mailbox.name" created with "$Mailbox.email" address"
-    #New-Mailbox -Alias $Mailbox.Name -PrimarySmtpAddress $Mailbox.Email -Shared
+    New-Mailbox -Name $Mailbox.Name -Alias $Mailbox.Name -PrimarySmtpAddress $Mailbox.Email -Shared
 
     #Assign permissions to mailboxes as created
     Foreach ($Member in $Members){
-        Write-Host "$Member addeded to "$Mailbox.name""
-        #Add-MailboxPermission -Identity $Mailbox.Name -User $member -AccessRights FullAccess,SendAs
-
+                
+        <# Permissions Note:
+        If you are creating a bunch of mailboxes, Outlook will only display the first 32 entries (including the user's mailbox and archive).This is why we have disabled AutoMapping. 
+        Adjust these lines as needed for appropriate AccessRights, AutoMapping settings, and if SendAs is required or not.
+        #>
+        Add-MailboxPermission -Identity $Mailbox.Name -User $member -AccessRights FullAccess -AutoMapping $false
+        Add-RecipientPermission -Identity $Mailbox.Name -Trustee $member -AccessRights SendAs -Confirm:$false
+        
     }
     
 
