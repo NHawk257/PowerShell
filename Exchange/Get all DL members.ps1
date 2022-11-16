13cd7b21f64e8a650fffcf575ed92fde6a7ee826
NHawk257 <90468946+NHawk257@users.noreply.github.com>
2022-11-16 09:31:26 -0700

    Added notes

--- Exchange/Get all DL members.ps1
+++ Exchange/Get all DL members.ps1
@@ -1,16 +1,20 @@
 ﻿Connect-ExchangeOnline
 
+#Create the initial CSV file. Adjust Path/Name as needed
 $csvfilename = ".\DL_Members.csv"
 New-Item $csvfilename -type file -force
 Add-Content $csvfilename "Name,Owner,Members"
 
+#Get all groups. Adjust as needed to filter or scope to an OU
 $Groups = Get-DistributionGroup | Select-Object -ExpandProperty PrimarySmtpAddress
 
 foreach ($Group in $groups){
 
-$Owner = Get-DistributionGroup $Group | Select-Object -ExpandProperty ManagedBy
-$members = Get-DistributionGroupMember $Group | Select-Object -ExpandProperty Name
+    #Get list of Owners and Members for each DL
+    $Owner = Get-DistributionGroup $Group | Select-Object -ExpandProperty ManagedBy
+    $Members = Get-DistributionGroupMember $Group | Select-Object -ExpandProperty Name
 
-Add-Content $csvfilename "$group,$owner,$members"
+    #Add details to CSV, one line per group
+    Add-Content $csvfilename "$group,$owner,$members"
 
 }
