da7df6ff576dcef93113767530634956a71e2280
Evan <90468946+NHawk257@users.noreply.github.com>
2023-02-17 14:02:56 -0700

    Added notes

--- AD/Block-trials.ps1
+++ AD/Block-trials.ps1
@@ -1,3 +1,7 @@
+<#Microsoft's setting found in the M365 Admin center does not block users from starting trials in "Power Platform" based apps including Project, Visio, and Dynamics.
+This script will run through all these products (based on the ID list provided at the moment by MS) and disable the ability for users to initiate trials.
+#>
+
 Connect-MSCommerce
 #Install this module if you don't have it, it is likely not used as of yet
 
