9f9113fdfafa6fd73f34367b7c05dd0072b8544b
Evan <90468946+NHawk257@users.noreply.github.com>
2023-02-27 09:10:15 -0700

    Updated to pull IDs from MS

--- AD/Block-trials.ps1
+++ AD/Block-trials.ps1
@@ -3,12 +3,11 @@ Microsoft's setting found in the M365 Admin center does not block users from sta
 This script will run through all these products (based on the ID list provided at the moment by MS) and disable the ability for users to initiate trials.
 #>
 
-Connect-MSCommerce
+Connect-MSCommerce 
 #Install this module if you don't have it, it is likely not used as of yet
 
-#ID list pulled from MS (https://learn.microsoft.com/en-us/microsoft-365/commerce/subscriptions/allowselfservicepurchase-powershell?view=o365-worldwide#view-a-list-of-self-service-purchase-products-and-their-status)
-$IDs = 'CFQ7TTC0LH2H','CFQ7TTC0KP0N','CFQ7TTC0KXG6','CFQ7TTC0KXG7','CFQ7TTC0L3PB','CFQ7TTC0HDB1','CFQ7TTC0HDB0','CFQ7TTC0HD33','CFQ7TTC0HD32','CFQ7TTC0PW0V','CFQ7TTC0HHS9',
-'CFQ7TTC0J203','CFQ7TTC0HX99','CFQ7TTC0LH05','CFQ7TTC0LH3N','CFQ7TTC0LHWP','CFQ7TTC0LHVK','CFQ7TTC0LHWM'
+#Get a list of all ProductIDs with the ServiceService policy assigned (enabled and disabled)
+$IDs = Get-MSCommerceProductPolicies AllowSelfServicePurchase | Select-Object -ExpandProperty ProductID -ErrorAction SilentlyContinue
 
 Foreach ($ID in $IDs){
 
