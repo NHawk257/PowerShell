43f353e0ee0e21a93be7a64a4b0dfed3c5b4da00
Evan <90468946+NHawk257@users.noreply.github.com>
2023-08-30 09:35:32 -0600

    Updated to use SMTP address

--- Exchange/Get-SharedMailboxMembers.ps1
+++ Exchange/Get-SharedMailboxMembers.ps1
@@ -1,14 +1,14 @@
-#Get a list of all members of a DL based on an inputted list of mailboxes
+#Get a list of all members of a Shared Mailbox based on an inputted list of mailboxes
 
 $csvfilename = ".\SM_Members.csv"
 New-Item $csvfilename -type file -force
 Add-Content $csvfilename "Name,Members"
 
-$Mailboxes = Import-Csv .\EPS_Recipients.csv | ? {$_.type -eq "Shared Mailbox"}
+$Mailboxes = Import-Csv '.\Unlicsensed Shared Mailboxes.csv' #| Where-Object {$_.type -eq "Shared Mailbox"}
 
 foreach ($Mailbox in $Mailboxes){
 
-    $Members = Get-MailboxPermission $Mailbox.Alias | ? {$_.AccessRights -eq "FullAccess"} | Select -ExpandProperty user
+    $Members = Get-MailboxPermission $Mailbox.PrimarySMTPAddress | Where-Object {$_.AccessRights -eq "FullAccess"} | Select-Object -ExpandProperty user
     $Name = $Mailbox.DisplayName
 
     Add-Content $csvfilename "$Name,$members"
