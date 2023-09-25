71b32b87e5a9b743cbd5de49415bfc017830f10d
Evan <90468946+NHawk257@users.noreply.github.com>
2023-09-25 11:24:42 -0600

    Added Error Action

--- Exchange/Find Shared Mailbox Permission by user.ps1
+++ Exchange/Find Shared Mailbox Permission by user.ps1
@@ -1,11 +1,11 @@
 Connect-ExchangeOnline
 
-$mailboxes = Get-Mailbox -ResultSize Unlimited -RecipientTypeDetails SharedMailbox | Select-Object  -ExpandProperty PrimarySMTPAddress
 $user =  Read-Host -Prompt "Please provide a user to check permissions for"
+$mailboxes = Get-Mailbox -ResultSize Unlimited -RecipientTypeDetails SharedMailbox | Select-Object  -ExpandProperty PrimarySMTPAddress
 
 foreach($mailbox in $mailboxes){
 
-    $Perms = Get-MailboxPermission $mailbox -User $user
+    $Perms = Get-MailboxPermission $mailbox -User $user -ErrorACtion SilentlyContinue
 
     If ($Perms -ne $Null){
         
