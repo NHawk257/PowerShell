c3c915f0eb4aeb7c28d2c777e37cb7aba5f13a7f
Evan <90468946+NHawk257@users.noreply.github.com>
2023-11-22 17:24:30 -0600

    Added count variables

--- Exchange/Get-Fake-SMB.ps1
+++ Exchange/Get-Fake-SMB.ps1
@@ -3,22 +3,25 @@ Connect-ExchangeOnline
 #Create the CSV file with headers
 $csvfilename = ".\Fake_Shared_Mailbox_report.csv"
 New-Item $csvfilename -type file -force
-Add-Content $csvfilename "Display Name,Alias,Primary SMTP,RecipientTypeDetails"
+Add-Content $csvfilename "Display Name,Alias,Primary SMTP,RecipientTypeDetails,Number of Users"
 
 #Get user type mailboxes. Adjust filtering as required
 $Users = Get-Mailbox -ResultSize 1000 -RecipientTypeDetails UserMailbox
 
 #Check each user type mailbox for sharing permissions. If they have permissions, add them to the CSV Report
 Foreach ($User in $Users){
+    #Reset variable as we're using this for our IF/ELSE statement
     $Perms = $Null
-        $Perms = Get-MailboxPermission -Identity $User.PrimarySMTPaddress | Where-Object {$_.User -notlike "*NT Auth*"}
-        If ($Perms -ne $Null){
+        #Counting number of unique identity entries
+        $Perms = Get-MailboxPermission -Identity $User.PrimarySMTPaddress | Where-Object {$_.User -notlike "*NT Auth*"} | Measure-Object -Property Identity
+        If ($Perms.count -gt 0){
             $DisplayName    =   $User.DisplayName
             $Alias          =   $User.Alias
             $PrimarySMTP    =   $User.PrimarySMTPAddress
             $RecipientType  =   $User.RecipientTypeDetails
+            $UniqueAccess   =   $Perms.Count
         
-        Add-Content $csvfilename "$DisplayName,$Alias,$PrimarySMTP,$RecipientType"
+        Add-Content $csvfilename "$DisplayName,$Alias,$PrimarySMTP,$RecipientType,$UniqueAccess"
     }
         else{}
 }
