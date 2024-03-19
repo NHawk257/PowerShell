770cc117dda06935ec1be64bfcaa4adc6f20fd0b
Evan <90468946+NHawk257@users.noreply.github.com>
2024-03-19 13:47:13 -0600

    Cleaned up, optimized

--- Exchange/Get all DL members.ps1
+++ Exchange/Get all DL members.ps1
@@ -6,14 +6,15 @@ New-Item $csvfilename -type file -force
 Add-Content $csvfilename "Name,Owner,Members"
 
 #Get all groups. Adjust as needed to filter or scope to an OU
-#$Groups = Get-DistributionGroup | Select-Object -ExpandProperty PrimarySmtpAddress
+$Groups = Get-DistributionGroup -ResultSize Unlimited -WarningAction SilentlyContinue | Where-Object {$_.Name -like "*catrents*"} | Select-Object -ExpandProperty PrimarySmtpAddress
+Write-Host $groups.count "groups found, starting report generation."
 
 foreach ($Group in $groups){
 
     #Get list of Owners and Members for each DL
-    $Owner = Get-Group $Group.Alias | Select-Object -ExpandProperty Owners
-    $Members = Get-Group $Group.Alias | Select-Object -ExpandProperty Members
-    $Name = $Group.DisplayName
+    $Owner = Get-DistributionGroup $Group | Select-Object -ExpandProperty ManagedBy
+    $Members = Get-DistributionGroupMember $Group | Select-Object -ExpandProperty DisplayName
+    $Name = Get-DistributionGroup $Group | Select-Object -ExpandProperty DisplayName
 
     #Add details to CSV, one line per group
     Add-Content $csvfilename "$Name,$owner,$members"
